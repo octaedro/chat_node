0d2738ab73455acded7b128e9f8062a53f9ab486
fernando <fernando@evimed-desktop.(none)>
2014-11-11 17:04:30 -0200

    local4

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ io.on('connection', function(socket) {
 /**
  * Iniciamos la aplicaciÃ³n en el puerto 3000
  */
-http.listen(3000, function() {
+http.listen(3000,'0.0.0.0', function() {
   console.log('listening on *:3000');
 });
