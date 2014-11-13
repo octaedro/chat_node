375610993be5f6b1ed9e0a3187c5879e689f65e7
camilocaminada <caminadacamilo@gmail.com>
2014-11-13 11:51:03 -0200

    vxcvxz

--- views/js/scriptnuestro.js
+++ views/js/scriptnuestro.js
@@ -1,3 +1,7 @@
+var app  = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+
 var socket = io(); // Inicializamos socketIO en el cliente
 
     /**
