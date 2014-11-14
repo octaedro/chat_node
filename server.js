7abbb88bf55f5301fb33b58629e33de2cc10e2e0
camilocaminada <caminadacamilo@gmail.com>
2014-11-14 13:37:25 -0200

    reloj

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ io.on('connection', function(socket) {
   });
 
   socket.on('reloj', function(msg) {
-    reloj(0);
+    reloj(60);
   });
 
   socket.on('votA', function() {
@@ -113,8 +113,8 @@ http.listen(3000, function() {
 
 function reloj(i){    
     setInterval(function(){
-      if(i<10){
-        io.emit('mostrar_reloj',++i); 
+      if(i<60){
+        io.emit('mostrar_reloj',--i); 
       }
       else{
         clearInterval();
